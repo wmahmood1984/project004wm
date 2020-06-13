3f21097ac26e980263aff71d856127298ae5977b
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-06-14 00:23:52 +0500

    first commit

---
+++ src/Room.js
@@ -0,0 +1,37 @@
+import React, { useState }from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function Room() {
+    let [isLIt, setLIt] = useState(false);
+    let [age, setAge] = useState(35)
+
+    function updateclick(){
+        console.log("button clicked")
+        setLIt(!isLIt);
+        
+    }
+    function incAge(){
+        console.log("age button clicked")
+        
+        setAge(++age)
+    }
+  return (
+    <div>This Room is  {isLIt? "lit": "dark"}
+    <br/>
+    age = {age}
+    <br/>
+    <button onClick={updateclick}>Toggle Light</button>
+    <br/>
+    <button onClick={() =>{
+        console.log("age button clicked")
+        
+        setAge(++age)
+    }}>inc age</button>
+    </div>
+  );
+}
+
+export default Room;
